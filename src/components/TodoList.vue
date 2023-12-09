cb6683b7de2b2265dc9489111e3ba59817f6b0e8
Seunghye Jeon <ivory0130@gmail.com>
2023-12-09 21:33:04 +0900

    부모컴포넌트에서 porp으로 자식컴포넌트로 보내기

---
+++ src/components/TodoList.vue
@@ -0,0 +1,47 @@
+<template>
+  <div 
+      v-for="(todo,index) in todos"
+      :key="todo.id"
+      class="card mt-2"
+  >
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
+          <input 
+             class="form-check-input" 
+             type="checkbox"
+             v-model="todo.completed"
+          >
+          <label  
+              class="form-check-label"
+              :class ="{ todo: todo.completed }"
+          >
+            {{ todo.subject }}
+          </label>
+        </div>
+        <div>
+          <button 
+            class="btn btn-danger btn-sm"
+            @click="deleteTodo(index)"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    //App.vue에서 보낸 todos(이름):보내는값의 이름 적어주어 사용하기
+    props: {
+        todos:{
+            type :Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
