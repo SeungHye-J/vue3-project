8dbd2e2f7c6bebf9dc67535819f7ed531364fcd8
Seunghye Jeon <ivory0130@gmail.com>
2023-12-11 19:34:24 +0900

    async/await 를 사용하여 함수를 비동기로 정의하기

--- src/App.vue
+++ src/App.vue
@@ -39,26 +39,20 @@ export default {
     const todos = ref([]);
     const error = ref('');
 
-    const addTodo = (todo) => {
+    const addTodo = async (todo) => {
       error.value = '';
       //데이터베이스에 todo를 저장한 후 Array추가하기
-      axios.post('http://localhost:3000/todos',{
-        subject: todo.subject,
-        completed: todo.completed
-        //id는 자동으로 생성해줌
-      }).then(res => {
-        //요청을 끝내고 응답이 왔을때 실행되는 부분.
-         console.log(res);
+      try{
+        const res = await axios.post('http://localhost:3000/todos',{
+          subject: todo.subject,
+          completed: todo.completed
+          //id는 자동으로 생성해줌
+        });
         todos.value.push(res.data);
-      }).catch(err => {
-        //요청이 실패했을경우
-         console.log(err);
-         error.value = 'Somthing went wrong.';
-      });
-      //자바스크립트에서 요청이 비동기적으로 일어나게됨. axios post로
-      // 요청을 보내고 응답이 오기 전에 그 다음으로 넘어가게됨. 그래서
-      //then 키워드를 통해서 요청이 끝났을때,응답이왔을때 실행될 수 있
-      //도록 해주어야함.
+      }catch(err){
+        console.log(err);
+        error.value = 'Somthing went wrong.';
+      }
      
     }
 
