b3d794e0ef13d08d83c51953a2e5cff7780e8ac9
Seunghye Jeon <ivory0130@gmail.com>
2023-12-11 19:59:53 +0900

    todo 데이터 db에서 삭제하기

--- src/App.vue
+++ src/App.vue
@@ -74,9 +74,19 @@ export default {
       
     }
 
-    const deleteTodo = (index) => {
-      todos.value.splice(index,1);
-    }
+    const deleteTodo = async (index) => {
+      error.value = '';
+      const id = todos.value[index].id;
+      try{
+        await axios.delete('http://localhost:3000/todos/' + id);
+        
+        todos.value.splice(index,1);
+      }catch(err){
+        console.log(err);
+        error.value = 'Somthing went wrong.';
+      }
+
+    };
 
     const searchText = ref('');
     const filteredTodos = computed(()=>{
