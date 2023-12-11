b119ffaeb47bef4570c20df6d3cd8ee1c314d67c
Seunghye Jeon <ivory0130@gmail.com>
2023-12-11 19:04:40 +0900

    todo 생성시 db에 저장하기

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     >
     <hr/>
     <TodoSimpleForm @add-todo="addTodo"/>
-    
+    <div>{{ error }}</div>
     <div v-if="!filteredTodos.length">
       There is nothing to display
     </div>
@@ -27,6 +27,7 @@
 import { ref, computed } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
 import TodoList from './components/TodoList.vue'
+import axios from 'axios';
 
 export default {
   components:{
@@ -36,10 +37,28 @@ export default {
   setup() {
    
     const todos = ref([]);
+    const error = ref('');
 
     const addTodo = (todo) => {
-     
-      todos.value.push(todo);
+      error.value = '';
+      //데이터베이스에 todo를 저장한 후 Array추가하기
+      axios.post('http://localhost:3000/todos',{
+        subject: todo.subject,
+        completed: todo.completed
+        //id는 자동으로 생성해줌
+      }).then(res => {
+        //요청을 끝내고 응답이 왔을때 실행되는 부분.
+         console.log(res);
+        todos.value.push(res.data);
+      }).catch(err => {
+        //요청이 실패했을경우
+         console.log(err);
+         error.value = 'Somthing went wrong.';
+      });
+      //자바스크립트에서 요청이 비동기적으로 일어나게됨. axios post로
+      // 요청을 보내고 응답이 오기 전에 그 다음으로 넘어가게됨. 그래서
+      //then 키워드를 통해서 요청이 끝났을때,응답이왔을때 실행될 수 있
+      //도록 해주어야함.
      
     }
 
@@ -72,6 +91,7 @@ export default {
        toggleTodo,
        searchText,
        filteredTodos,
+       error,
     };
   }
 
