573a4e33d09831b9bef2adfc45cf042f9f93c860
Seunghye Jeon <ivory0130@gmail.com>
2023-12-12 19:58:52 +0900

    router-link,to를 사용하여 페이지 간 리로드 방지

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">무럭무럭 나무</a>
-    
+    <!--리로딩 없이 하기위해 a,href -> router-link,to 를 사용함 -->
+    <router-link class="navbar-brand" to="/">
+      무럭무럭 나무
+    </router-link>
+
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Todos <span class="sr-only">(current)</span></a>
+        <router-link class="nav-link" to="/todos">
+          Todos
+        </router-link>
       </li>
     </ul>
   </nav>
+
   <router-view/>
 </template>
 
