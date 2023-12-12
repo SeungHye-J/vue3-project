1159346e1a74b8aa9e05dd991ef62a1540a5569c
Seunghye Jeon <ivory0130@gmail.com>
2023-12-12 19:31:15 +0900

    페이지추가를 위해 router설정

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue' //vue에서 createApp 함수를 가져와서
 import App from './App.vue'  //최상위 컴포넌트를 App에 넣어주고
+import router from './router';
 
-createApp(App).mount('#app') //최상위 컴포넌트를 id가 app인 곳에 넣어주는 것임.
+createApp(App).use(router).mount('#app') //최상위 컴포넌트를 id가 app인 곳에 넣어주는 것임.
 //app컴포넌트 안에는 무엇이 있을까?? App.vue로 가기
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter,createWebHistory } from 'vue-router';
+import Home from '../pages/index.vue';
+import Todos from '../pages/todos/index.vue';
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/', //url
+            name: 'Home',
+            component: Home
+        },//하나의 객체가 하나의 라우트
+        {
+            path: '/todos',
+            name: 'Todos',
+            component: Todos
+        }
+    ] //url 정보가 들어가게됨
+});
+
+//1. /home , 2. /todos , 3. /todos/create , 4. /todos/:id
+export default router;
