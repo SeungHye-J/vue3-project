3c533a5dce78f711685776acaac174d822e138ce
Seunghye Jeon <ivory0130@gmail.com>
2023-12-14 17:32:33 +0900

    수정페이지 추가 및 이동하기

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <!--리로딩 없이 하기위해 a,href -> router-link,to 를 사용함 -->
-    <router-link class="navbar-brand" to="/">
+    <router-link class="navbar-brand" :to="{ name: 'Home' }">
       무럭무럭 나무
     </router-link>
 
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <router-link class="nav-link" to="/todos">
+        <router-link class="nav-link" :to="{ name:'Todos'}">
           Todos
         </router-link>
       </li>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,7 +4,10 @@
       :key="todo.id"
       class="card mt-2"
   >
-      <div class="card-body p-2 d-flex align-items-center">
+      <div 
+        class="card-body p-2 d-flex align-items-center"
+        @click="moveToPage(todo.id)"
+      >
         <div class="form-check flex-grow-1">
           <input 
              class="form-check-input" 
@@ -32,6 +35,7 @@
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
 export default {
     //App.vue에서 보낸 todos(이름):보내는값의 이름 적어주어 사용하기
     props: {
@@ -42,7 +46,7 @@ export default {
     },
     emits: ['toggle-todo','delete-todo'], //emit명 배열에 담아서 보내주면 경고사라짐
     setup(props,{emit}) { //context - > {emit}
-
+        const router = useRouter();
         const toggleTodo = (index) => {
             emit('toggle-todo' , index);
         }
@@ -51,9 +55,23 @@ export default {
             emit('delete-todo',index);
         }
 
+        const moveToPage = (todoId) => {
+          console.log(todoId);
+          //페이지이동방법1
+          // router.push('/todos/' +todoId); // 원하는페이지로 보내줌
+          //페이지이동방법2
+          router.push({
+            name: 'Todo',
+            params: {
+              id: todoId
+            }
+          })
+        }
+
         return {
             toggleTodo,
-            deleteTodo
+            deleteTodo,
+            moveToPage
         }
 
     }
