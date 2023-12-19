3d7013f4a5bddddc53e3691dd096b03d90eae16a
Seunghye Jeon <ivory0130@gmail.com>
2023-12-19 19:05:30 +0900

    모달창 delete 기능 추가

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -3,13 +3,15 @@
     <div class="modal-dialog">
         <div class="modal-content">
         <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            <span @click="onClose">&times;</span>
+            <h5 class="modal-title">
+                Delete Todo 
+            </h5>
+            <button type="button" class="close">
+                <span @click="onClose">&times;</span>
             </button>
         </div>
         <div class="modal-body">
-            ...
+            Are you sure you want to delete the todo?
         </div>
         <div class="modal-footer">
             <button 
@@ -19,7 +21,13 @@
             >
               Close
             </button>
-            <button type="button" class="btn btn-primary">Save changes</button>
+            <button 
+                type="button"
+                class="btn btn-danger"
+                @click="onDelete"
+            >
+                Delete
+            </button>
         </div>
         </div>
     </div>
@@ -34,9 +42,13 @@ export default {
             emit('close');
         }
 
+        const onDelete = () => {
+            emit('delete')
+        }
+
         return {
             onClose,
-
+            onDelete,
         }
     }
 
